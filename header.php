0d3f1d05797d5cc0a719d06c92d709c03a5bb409
Mari-Ogawa <awashimauranaibu@gmail.com>
2018-08-26 16:09:31 +0900

    lesson8

--- header.php
+++ header.php
@@ -1,11 +1,63 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
+<head prefix="og: http://ogp.me/ns#">
 <meta charset="utf-8">
 <meta name="viewport"
 content="width=device-width, initial-scale=1.0 ">
+
+<?php if(is_tag() || is_date() || is_search() || is_404()) : ?>
+  <meta name="robots" content="noindex"/>
+<?php endif; ?>
+
+<meta property="og:site_name" content="<?php bloginfo( 'name' ); ?>">
+<meta property="og:locale" content="ja_JP">
+
+<!--個別ページ用のmetaデータ-->
+<?php if( is_single() || is_page() ): ?>
+  <?php setup_postdata($post) ?>
+  <meta name="description" content="<?php echo strip_tags( get_the_excerpt() ); ?>"/>
+
+  <?php if ( has_tag() ): ?>
+    <?php $tags = get_the_tags();
+    $kwds = array();
+    foreach($tags as $tag){
+      $kwds[] = $tag->name;
+    } ?>
+    <meta name="keywords" content="<?php echo implode( ',',$kwds ); ?>">
+  <?php endif; ?>
+  <meta property="og:type" content="article">
+  <meta property="og:title" content="<?php the_title(); ?>">
+  <meta property="og:url" content="<?php the_permalink(); ?>">
+  <meta property="og:description" content="<?php echo strip_tags( get_the_excerpt() ); ?>">
+  <?php if( has_post_thumbnail() ): ?>
+    <?php $postthumb = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' ); ?>
+    <meta property="og:image" content="<?php echo $postthumb[0]; ?>">
+  <?php endif; ?>
+<?php else: ?><!--個別ページ以外のメタデータ-->
+  <meta name="description" content="<?php bloginfo( 'description' ); ?>">
+  <?php $allcats = get_categories();
+  $kwds = array();
+  foreach($allcats as $allcat) {
+    $kwds[] = $allcat->name;
+  } ?>
+  <meta name="keywords" content="<?php echo implode( ',',$kwds ); ?>">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="<?php bloginfo( 'name' ); ?>">
+  <?php
+  $http = is_ssl() ? 'https' . '://' : 'http' . '://';
+  $url = $http . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+  ?>
+  <meta property="og:url" content="<?php echo $url; ?>">
+  <meta property="og:description" content="<?php bloginfo( 'description' ) ?>">
+  <!-- <meta property="og:image" content="表示したい画像のパス"> -->
+<?php endif; ?>
+
 <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
+<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri() ?>/images/webclipicon.png" />
+<link rel="shortcut icon" href="<?php echo get_template_directory_uri() ?>/images/favicon.ico" />
+
 
 <?php wp_head(); ?>
 </head>
